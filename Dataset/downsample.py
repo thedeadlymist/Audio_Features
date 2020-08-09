dd84f5fff58421d76f24342af45b88c7a0dbff2d
vybhav72954 <49750343+vybhav72954@users.noreply.github.com>
2020-08-09 12:57:48 +0530

    Create downsample.py

---
+++ Dataset/downsample.py
@@ -0,0 +1,23 @@
+'''
+TODO
+
+Rename File Properly
+Check if Directory exists
+Chechk th change in file size
+'''
+
+import os
+import soundfile
+from glob import glob
+import librosa as lr
+
+data_dir = 'C:\\Users\\asus\\Desktop\\Features\\Song\\*.wav'
+
+audio_files = glob(data_dir)
+len(audio_files)
+
+
+for file in range(0, len(audio_files), 1):
+    audio, sfreq = lr.load(audio_files[file], sr=16000, mono=True)
+    soundfile.write('C:\\Users\\asus\\Desktop\\Features\\Song1\\%s.wav' % str(file+1).zfill(3), audio, 16000, subtype='PCM_24')
+    print ('Done {}'.format(str(file)))
