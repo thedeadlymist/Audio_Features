1d3d5fc5e86b9e97f314711875b29f6a82215993
vybhav72954 <49750343+vybhav72954@users.noreply.github.com>
2020-08-09 12:15:43 +0530

    Added an example

--- Dataset/audio_parameteres.py
+++ Dataset/audio_parameteres.py
@@ -3,3 +3,5 @@ import wave
 
 with wave.open("C:\\Users\\asus\\Desktop\\Features\\Teri Mitti.wav") as file:   # Song Directory
     print(file.getparams())
+    
+# Example - _wave_params(nchannels=2, sampwidth=2, framerate=32000, nframes=10051200, comptype='NONE', compname='not compressed')
